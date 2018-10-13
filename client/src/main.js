c583991d62e9000b6acc7489ed78ca890e9c5663
Sam Gates <sam@covertnine.com>
2018-10-13 11:44:14 -0500

    a commit

--- client/src/main.js
+++ client/src/main.js
@@ -11,6 +11,7 @@ import io from 'socket.io-client'
  */
 var socket = io.connect('http://localhost:5000');
 var playerNum = null;
+console.log("cool");
 
 socket.on('connect', function() {
     console.log('Websocket connected!');
