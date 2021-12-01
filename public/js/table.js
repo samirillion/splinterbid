b683e454bd03be177b17a59e45fd426048d884c4
Sam Gates <samirillion@protonmail.com>
2021-12-01 15:58:33 -0800

    commit before refactoring data object

--- public/js/table.js
+++ public/js/table.js
@@ -1,43 +1,6 @@
 /**
- * Super cool description - Socket Code
+ * Application Code
  */
- var socket = io.connect('http://' + document.domain + ':' + location.port);
- var playerNum = null;
- 
- socket.on('connect', function() {
-     console.log('Websocket connected!');
- });
- 
- socket.on('error', function(msg) {
-     console.error(msg['error']);
- });
- 
- socket.on('room_made', function(msg) {
-     var gameID = msg['gameID'];
-     createButton('Join Room ' + gameID, makeJoinRoomFunction(gameID));
- });
- 
- socket.on('player_num', function(msg) {
-     playerNum = msg['playerNum'];
-     console.log('Received player num: ' + playerNum);
- });
- 
- socket.on('initial_connection', function(msg) {
-     var gameIDs = msg['gameIDs'];
-     console.log(msg)
-     createButton('Make Room!', createRoom);
-     for (i=0; i < gameIDs.length; i++){
-         var gameID = gameIDs[i];
-
-         createButton('Join Room ' + gameID, makeJoinRoomFunction(gameID));
-     }
- });
- 
- socket.on('game_state', function(msg) {
-     var gameState = JSON.parse(msg);
-     console.log('Message received.');
-     console.log(gameState);
- });
  
  function makeJoinRoomFunction(gameID) {
      return function () {
@@ -55,6 +18,12 @@
  * Application Code
  */
 function render_hands() {
+  var hands = document.querySelectorAll(".hand");
+  [...hands].forEach(function(hand) {
+    [...hand.childNodes].forEach(function(card) {
+      card.remove();
+    })
+  });
   Object.keys(CARDINAL_MATRIX).forEach(function (seat, index) {
     build_hand(seat, index);
   });
@@ -219,4 +188,44 @@ function init() {
   update_bid_panel();
 }
 
-init();
+
+/**
+ * Super cool description - Socket Code
+ */
+ var socket = io.connect('http://' + document.domain + ':' + location.port);
+ var playerNum = null;
+ 
+ socket.on('connect', function() {
+     console.log('Websocket connected!');
+ });
+ 
+ socket.on('error', function(msg) {
+     console.error(msg['error']);
+ });
+ 
+ socket.on('room_made', function(msg) {
+     var gameID = msg['gameID'];
+     createButton('Join Room ' + gameID, makeJoinRoomFunction(gameID));
+ });
+ 
+ socket.on('player_num', function(msg) {
+     playerNum = msg['playerNum'];
+     console.log('Received player num: ' + playerNum);
+ });
+ 
+ socket.on('initial_connection', function(msg) {
+     var gameIDs = msg['gameIDs'];
+     console.log(msg)
+     createButton('Make Room!', createRoom);
+     for (i=0; i < gameIDs.length; i++){
+         var gameID = gameIDs[i];
+
+         createButton('Join Room ' + gameID, makeJoinRoomFunction(gameID));
+     }
+ });
+ 
+ socket.on('game_state', function(msg) {
+     var gameState = JSON.parse(msg);
+     
+     init();
+ });
